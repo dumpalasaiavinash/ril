38fd8c557a3fde8e18adcd2a92df0bbe9528943d
dumpalasaiavinash <dumpalasaiavinash@gmail.com>
2023-12-04 12:23:10 +0530

    none

--- app/get_similarity.py
+++ app/get_similarity.py
@@ -9,6 +9,7 @@ def find_most_similar_word(text):
     import difflib
     from domain_specific_words import wells
     wells = map(lambda x:x.lower(), wells)
+    wells = list(wells)
 
     # Extract the word after "well" in the sample sentence
     # words_after_well = sample_sentence.split("well")[1].split()[0]
@@ -20,7 +21,8 @@ def find_most_similar_word(text):
         # print(i)
         if i not in wells:
             most_similar_word = difflib.get_close_matches(i, wells, n=1, cutoff=0.7)
-            # print(most_similar_word)
+            print(most_similar_word,i)
+            # print(wells)
             if most_similar_word:
                 text = text.replace(i,most_similar_word[0])
             else:
@@ -32,7 +34,7 @@ def find_most_similar_word(text):
 
 
 
-# processed_text = "who is the age of the well KGD6Os in the well KGD6D26MA"
+# processed_text = "who is the age of the well python in the well pythonsd"
 # # Find the most similar word
 # result = find_most_similar_word(processed_text)
 
